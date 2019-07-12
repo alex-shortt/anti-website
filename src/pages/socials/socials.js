62d164e50178b961fa5bb47b3e1f33f48c6e9a75
Alex Shortt <alexander.shortt@gmail.com>
2019-07-12 13:36:15 -0700

    insert first ig posts

--- src/pages/socials/socials.js
+++ src/pages/socials/socials.js
@@ -5,18 +5,18 @@ import getPostInfo from "../../common/instagram.js"
 initAnti3D()
 
 // post shortcodes they want to display
-const posts = ["Bu4zBPLA7RB", "BtB8LkrlvEu", "BtB4StKly7G"]
+const posts = ["BztyZm3nUc9", "BzeL7G8H-6l", "By3rQ4Wp9Wc", "ByqTI4LHOSF"]
 
-// async function fillInstaData() {
-//   for (const post of posts) {
-//     let postData = await getPostInfo(post)
+async function fillInstaData() {
+  for (const post of posts) {
+    let postData = await getPostInfo(post)
 
-//     var container = $(".socials-left-instafeed")
-//     var post = $("<img class='socials-left-instafeed-instapost'>")
-//     post.click(() => window.open(postData.url))
-//     post.attr("src", postData.img)
-//     post.appendTo($(container))
-//   }
-// }
+    var container = $(".socials-left-instafeed")
+    var picture = $("<img class='socials-left-instafeed-instapost'>")
+    picture.click(() => window.open(postData.url))
+    picture.attr("src", postData.img)
+    picture.appendTo($(container))
+  }
+}
 
-// fillInstaData()
+fillInstaData()
