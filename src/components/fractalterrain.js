102a695215ca33af3633a034ec19a14c6bfba166
Alex Shortt <alexander.shortt@gmail.com>
2019-07-17 09:54:25 -0700

    refactor all components back to uncompiled state

--- src/components/fractalterrain.js
+++ src/components/fractalterrain.js
@@ -1,234 +1,209 @@
-import _regeneratorRuntime from "babel-runtime/regenerator";
+import SVG from "../common/SVG.js"
+import { delay } from "../common/promises.js"
 
-var svgInit = function () {
-  var _ref = _asyncToGenerator( /*#__PURE__*/_regeneratorRuntime.mark(function _callee2() {
-    var slowDraw = function () {
-      var _ref2 = _asyncToGenerator( /*#__PURE__*/_regeneratorRuntime.mark(function _callee(w) {
-        return _regeneratorRuntime.wrap(function _callee$(_context) {
-          while (1) {
-            switch (_context.prev = _context.next) {
-              case 0:
-                prog++;
-                drawTop(w, ctxTop);
-                drawBot(w, ctxBot);
-                if (w <= size) requestAnimationFrame(function () {
-                  slowDraw(w + 1);
-                });
+async function svgInit() {
+  let canvasTop = $("#fractal-terrain-top").get(0)
+  let ctxTop = canvasTop.getContext("2d")
+  let canvasBot = $("#fractal-terrain-bottom").get(0)
+  let ctxBot = canvasBot.getContext("2d")
+  let width = $("body").width()
+  let height = $("body").height()
+  let prog = 0
 
-              case 4:
-              case "end":
-                return _context.stop();
-            }
-          }
-        }, _callee, this);
-      }));
+  /////////////////////////////////////
+  const size = Math.pow(2, 8)
+  const water = 1 // 0 for no water
+  let seed = (Math.random() * 100000) | 0
+  const hmap = []
+  let topLine
+  let botLine
+  let topPen = false
+  let botPen = false
+  /////////////////////////////////////
 
-      return function slowDraw(_x) {
-        return _ref2.apply(this, arguments);
-      };
-    }();
+  console.log("fractal random seed: " + seed)
+  function random() {
+    seed = (seed * 16807) % 2147483647
+    return (seed - 1) / 2147483646
+  }
 
-    var canvasTop, ctxTop, canvasBot, ctxBot, width, height, prog, size, water, seed, hmap, topLine, botLine, topPen, botPen, random, setup, drawTop, drawBot, redrawFractal;
-    return _regeneratorRuntime.wrap(function _callee2$(_context2) {
-      while (1) {
-        switch (_context2.prev = _context2.next) {
-          case 0:
-            redrawFractal = function redrawFractal() {
-              ctxTop.fillStyle = "#000000";
-              ctxTop.fillRect(0, 0, $("body").width() * 2, $("body").height());
-              ctxBot.fillStyle = "#000000";
-              ctxBot.fillRect(0, 0, $("body").width() * 2, $("body").height());
+  function setup() {
+    canvasTop.width = width * 2
+    canvasTop.height = height
+    canvasBot.width = width * 2
+    canvasBot.height = height
 
-              width = $("body").width() * 2;
-              height = $("body").height();
-
-              canvasTop.width = width * 2;
-              canvasTop.height = height;
-              canvasBot.width = width * 2;
-              canvasBot.height = height;
-
-              botLine = new Float32Array(size + 1);
-              topLine = new Float32Array(size + 1);
-
-              topPen = false;
-              botPen = false;
-
-              for (var w = 0; w <= size; w++) {
-                drawTop(w, ctxTop);
-                drawBot(w, ctxBot);
-              }
-            };
-
-            drawBot = function drawBot(w, thisContext) {
-              var locSize = 256;
-              var r = locSize / size;
-              var k = 0;
-              botPen = false;
-              thisContext.strokeStyle = "#c1c1c1";
-              for (var z = 0; z <= size; z++) {
-                var xe = r * z * 1; //smoothness of mountains
-                var ye = r * 0.66 * w + hmap[z][w] * 1; //peak
-                if (ye <= botLine[z] || hmap[z][w] === 0 && w / water !== (w / water | 0)) {
-                  if (botPen) thisContext.stroke();
-                  botPen = false;
-                } else {
-                  if (botPen === false) thisContext.moveTo(width * 2 * xe / locSize, height * (locSize - ye) / locSize);else thisContext.lineTo(width * 2 * xe / locSize, height * ye / locSize);
-                  if (!botPen) thisContext.beginPath();
-                  botPen = true;
-                  botLine[z] = ye;
-                }
-              }
-              return w < size;
-            };
-
-            drawTop = function drawTop(w, thisContext) {
-              var locSize = 256;
-              var r = locSize / size;
-              var k = 0;
-              topPen = false;
-              thisContext.strokeStyle = "#c1c1c1";
-              for (var z = 0; z <= size; z++) {
-                var xe = r * z * 1; //smoothness of mountains
-                var ye = r * 0.66 * w + hmap[z][w] * 1; //peak
-                if (ye <= topLine[z] || hmap[z][w] === 0 && w / water !== (w / water | 0)) {
-                  if (topPen) thisContext.stroke();
-                  topPen = false;
-                } else {
-                  if (topPen === false) thisContext.moveTo(width * 2 * xe / locSize, height * (locSize - ye) / locSize);else thisContext.lineTo(width * 2 * xe / locSize, height * (locSize - ye) / locSize);
-                  if (!topPen) thisContext.beginPath();
-                  topPen = true;
-                  topLine[z] = ye;
-                }
-              }
-              return w < size;
-            };
-
-            setup = function setup() {
-              canvasTop.width = width * 2;
-              canvasTop.height = height;
-              canvasBot.width = width * 2;
-              canvasBot.height = height;
-
-              var randomLevel = 60;
-              var nbits = size.toString(2).length - 1;
-              var rnd = function rnd() {
-                return randomLevel * (-1 + 2 * random());
-              };
-              botLine = new Float32Array(size + 1);
-              topLine = new Float32Array(size + 1);
-              for (var i = 0; i <= size; i++) {
-                hmap[i] = new Float32Array(size + 1);
-              }var t = 1;
-              var x = size / 2;
-              for (var s = 1; s <= nbits; s++) {
-                for (var v = 0; v <= size; v += 2 * x) {
-                  for (var n = 1; n <= t; n += 2) {
-                    hmap[n * x][v] = (hmap[(n - 1) * x][v] + hmap[(n + 1) * x][v]) / 2 + rnd();
-                    hmap[v][n * x] = (hmap[v][(n - 1) * x] + hmap[v][(n + 1) * x]) / 2 + rnd();
-                  }
-                }
-                for (var _n = 1; _n <= t; _n += 2) {
-                  for (var m = 1; m <= t; m += 2) {
-                    hmap[_n * x][m * x] = 0.25 * (hmap[_n * x + x][m * x] + hmap[_n * x - x][m * x] + hmap[_n * x][m * x + x] + hmap[_n * x][m * x - x]) + rnd();
-                  }
-                }
-                t = 2 * t + 1;
-                x /= 2;
-                randomLevel /= 2;
-              }
-
-              // remove flat areas
-              // for (let w = 0; w <= size; w++) {
-              //   for (let z = 0; z <= size; z++) {
-              //     if (hmap[w][z] < 0) hmap[w][z] = Math.abs(hmap[w][z]) //.25 * Math.abs(hmap[w][z])
-              //   }
-              // }
-
-              // mirror over y axis
-              // for (let w = size / 2; w <= size; w++) {
-              //   let oppos = size / 2 - (w - size / 2)
-              //   for (let z = 0; z <= size; z++) {
-              //     if (hmap[oppos][z]) {
-              //       hmap[oppos][z] = hmap[w][z]
-              //     }
-              //   }
-              // }
-
-              // //hmapInverse = hmap
-              // for (let w = size / 2; w <= size; w++) {
-              //   let oppos = size / 2 - (w - size / 2)
-              //   for (let z = 0; z <= size; z++) {
-              //     if (hmap[z][oppos]) {
-              //       hmap[z][oppos] = hmap[z][w]
-              //     }
-              //   }
-              // }
-            };
-
-            random = function random() {
-              seed = seed * 16807 % 2147483647;
-              return (seed - 1) / 2147483646;
-            };
-
-            canvasTop = $("#fractal-terrain-top").get(0);
-            ctxTop = canvasTop.getContext("2d");
-            canvasBot = $("#fractal-terrain-bottom").get(0);
-            ctxBot = canvasBot.getContext("2d");
-            width = $("body").width();
-            height = $("body").height();
-            prog = 0;
-
-            /////////////////////////////////////
-
-            size = Math.pow(2, 8);
-            water = 1; // 0 for no water
-
-            seed = Math.random() * 100000 | 0;
-            hmap = [];
-            topLine = void 0;
-            botLine = void 0;
-            topPen = false;
-            botPen = false;
-            /////////////////////////////////////
-
-            console.log("fractal random seed: " + seed);
-
-
-            setup();
-            slowDraw(0);
-
-          case 23:
-            if (!(prog < 256)) {
-              _context2.next = 28;
-              break;
-            }
-
-            _context2.next = 26;
-            return delay(1);
-
-          case 26:
-            _context2.next = 23;
-            break;
-
-          case 28:
-            window.onresize = redrawFractal;
-
-          case 29:
-          case "end":
-            return _context2.stop();
+    let randomLevel = 60
+    const nbits = size.toString(2).length - 1
+    const rnd = () => randomLevel * (-1 + 2 * random())
+    botLine = new Float32Array(size + 1)
+    topLine = new Float32Array(size + 1)
+    for (let i = 0; i <= size; i++) hmap[i] = new Float32Array(size + 1)
+    let t = 1
+    let x = size / 2
+    for (let s = 1; s <= nbits; s++) {
+      for (let v = 0; v <= size; v += 2 * x) {
+        for (let n = 1; n <= t; n += 2) {
+          hmap[n * x][v] =
+              (hmap[(n - 1) * x][v] + hmap[(n + 1) * x][v]) / 2 + rnd()
+          hmap[v][n * x] =
+              (hmap[v][(n - 1) * x] + hmap[v][(n + 1) * x]) / 2 + rnd()
         }
       }
-    }, _callee2, this);
-  }));
+      for (let n = 1; n <= t; n += 2) {
+        for (let m = 1; m <= t; m += 2) {
+          hmap[n * x][m * x] =
+              0.25 *
+              (hmap[n * x + x][m * x] +
+                  hmap[n * x - x][m * x] +
+                  hmap[n * x][m * x + x] +
+                  hmap[n * x][m * x - x]) +
+              rnd()
+        }
+      }
+      t = 2 * t + 1
+      x /= 2
+      randomLevel /= 2
+    }
 
-  return function svgInit() {
-    return _ref.apply(this, arguments);
-  };
-}();
+    // remove flat areas
+    // for (let w = 0; w <= size; w++) {
+    //   for (let z = 0; z <= size; z++) {
+    //     if (hmap[w][z] < 0) hmap[w][z] = Math.abs(hmap[w][z]) //.25 * Math.abs(hmap[w][z])
+    //   }
+    // }
 
-function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+    // mirror over y axis
+    // for (let w = size / 2; w <= size; w++) {
+    //   let oppos = size / 2 - (w - size / 2)
+    //   for (let z = 0; z <= size; z++) {
+    //     if (hmap[oppos][z]) {
+    //       hmap[oppos][z] = hmap[w][z]
+    //     }
+    //   }
+    // }
 
-import SVG from "../common/SVG.js";
-import { delay } from "../common/promises.js";
+    // //hmapInverse = hmap
+    // for (let w = size / 2; w <= size; w++) {
+    //   let oppos = size / 2 - (w - size / 2)
+    //   for (let z = 0; z <= size; z++) {
+    //     if (hmap[z][oppos]) {
+    //       hmap[z][oppos] = hmap[z][w]
+    //     }
+    //   }
+    // }
+  }
 
-export default svgInit;
+  function drawTop(w, thisContext) {
+    const locSize = 256
+    const r = locSize / size
+    let k = 0
+    topPen = false
+    thisContext.strokeStyle = "#c1c1c1"
+    for (let z = 0; z <= size; z++) {
+      let xe = r * z * 1 //smoothness of mountains
+      let ye = r * 0.66 * w + hmap[z][w] * 1 //peak
+      if (
+          ye <= topLine[z] ||
+          (hmap[z][w] === 0 && w / water !== ((w / water) | 0))
+      ) {
+        if (topPen) thisContext.stroke()
+        topPen = false
+      } else {
+        if (topPen === false)
+          thisContext.moveTo(
+              (width * 2 * xe) / locSize,
+              (height * (locSize - ye)) / locSize
+          )
+        else
+          thisContext.lineTo(
+              (width * 2 * xe) / locSize,
+              (height * (locSize - ye)) / locSize
+          )
+        if (!topPen) thisContext.beginPath()
+        topPen = true
+        topLine[z] = ye
+      }
+    }
+    return w < size
+  }
+
+  function drawBot(w, thisContext) {
+    const locSize = 256
+    const r = locSize / size
+    let k = 0
+    botPen = false
+    thisContext.strokeStyle = "#c1c1c1"
+    for (let z = 0; z <= size; z++) {
+      let xe = r * z * 1 //smoothness of mountains
+      let ye = r * 0.66 * w + hmap[z][w] * 1 //peak
+      if (
+          ye <= botLine[z] ||
+          (hmap[z][w] === 0 && w / water !== ((w / water) | 0))
+      ) {
+        if (botPen) thisContext.stroke()
+        botPen = false
+      } else {
+        if (botPen === false)
+          thisContext.moveTo(
+              (width * 2 * xe) / locSize,
+              (height * (locSize - ye)) / locSize
+          )
+        else
+          thisContext.lineTo(
+              (width * 2 * xe) / locSize,
+              (height * ye) / locSize
+          )
+        if (!botPen) thisContext.beginPath()
+        botPen = true
+        botLine[z] = ye
+      }
+    }
+    return w < size
+  }
+
+  function redrawFractal() {
+    ctxTop.fillStyle = "#000000"
+    ctxTop.fillRect(0, 0, $("body").width() * 2, $("body").height())
+    ctxBot.fillStyle = "#000000"
+    ctxBot.fillRect(0, 0, $("body").width() * 2, $("body").height())
+
+    width = $("body").width() * 2
+    height = $("body").height()
+
+    canvasTop.width = width * 2
+    canvasTop.height = height
+    canvasBot.width = width * 2
+    canvasBot.height = height
+
+    botLine = new Float32Array(size + 1)
+    topLine = new Float32Array(size + 1)
+
+    topPen = false
+    botPen = false
+
+    for (let w = 0; w <= size; w++) {
+      drawTop(w, ctxTop)
+      drawBot(w, ctxBot)
+    }
+  }
+
+  async function slowDraw(w) {
+    prog++
+    drawTop(w, ctxTop)
+    drawBot(w, ctxBot)
+    if (w <= size)
+      requestAnimationFrame(() => {
+        slowDraw(w + 1)
+      })
+  }
+
+  setup()
+  slowDraw(0)
+  while (prog < 256) {
+    await delay(1)
+  }
+  window.onresize = redrawFractal
+}
+
+export default svgInit
--- src/components/imgSlider.js
+++ src/components/imgSlider.js
@@ -1,146 +1,98 @@
-import _regeneratorRuntime from "babel-runtime/regenerator";
+async function initSlider() {
+  // Used to add a numeric id on slide creation to let us target the element later
+  var slideIndex = 0
+  // Tells us which slide we are on
+  var currentSlideIndex = 0
+  // An Array to hold all the slides
+  var slideArray = []
 
-var initSlider = function () {
-  var _ref = _asyncToGenerator( /*#__PURE__*/_regeneratorRuntime.mark(function _callee3() {
-    var slideIndex, currentSlideIndex, slideArray, Slide, slideObjs, prevSlide, nextSlide, counter, left, right;
-    return _regeneratorRuntime.wrap(function _callee3$(_context3) {
-      while (1) {
-        switch (_context3.prev = _context3.next) {
-          case 0:
-            nextSlide = function nextSlide() {
-              // Figure out what the next slide is
-              var nextSlideIndex;
-              // If we are at the last slide the next one is the first (zero based)
-              if (currentSlideIndex === slideArray.length - 1) {
-                nextSlideIndex = 0;
-              } else {
-                // Otherwise the next slide is the current one plus 1
-                nextSlideIndex = currentSlideIndex + 1;
-              }
+  function Slide(elemID) {
+    this.elem = $(elemID)
+  }
 
-              // Setup the next slide and current slide for animations
-              document.getElementById("slide" + nextSlideIndex).style.left = "100%";
-              document.getElementById("slide" + currentSlideIndex).style.left = 0;
+  // Creating our slide objects, you can make as many as you want
+  var slideObjs = $(".popup-slide")
+  slideObjs.each(function() {
+    $(this).attr("id", "slide" + slideIndex)
+    slideIndex++
+    var slideTemp = new Slide($(this).attr("id"))
+    slideArray.push(slideTemp)
+  })
 
-              // Add the appropriate animation class to the slide
-              document.getElementById("slide" + nextSlideIndex).setAttribute("class", "popup-slide singleSlide slideInRight");
-              document.getElementById("slide" + currentSlideIndex).setAttribute("class", "popup-slide singleSlide slideOutLeft");
+  document.getElementById("slide" + currentSlideIndex).style.left = 0
 
-              // Set current slide to the new current slide
-              currentSlideIndex = nextSlideIndex;
-            };
+  // Navigates to the previous slide in the list
+  function prevSlide() {
+    // Figure out what the previous slide is
+    var nextSlideIndex
+    // If we are at zero go to the last slide in the list
+    if (currentSlideIndex === 0) {
+      nextSlideIndex = slideArray.length - 1
+    } else {
+      // Otherwise the next one is this slide minus 1
+      nextSlideIndex = currentSlideIndex - 1
+    }
 
-            prevSlide = function prevSlide() {
-              // Figure out what the previous slide is
-              var nextSlideIndex;
-              // If we are at zero go to the last slide in the list
-              if (currentSlideIndex === 0) {
-                nextSlideIndex = slideArray.length - 1;
-              } else {
-                // Otherwise the next one is this slide minus 1
-                nextSlideIndex = currentSlideIndex - 1;
-              }
+    // Setup the next slide and current slide for animations
+    document.getElementById("slide" + nextSlideIndex).style.left = "-100%"
+    document.getElementById("slide" + currentSlideIndex).style.left = 0
 
-              // Setup the next slide and current slide for animations
-              document.getElementById("slide" + nextSlideIndex).style.left = "-100%";
-              document.getElementById("slide" + currentSlideIndex).style.left = 0;
+    // Add the appropriate animation class to the slide
+    document
+        .getElementById("slide" + nextSlideIndex)
+        .setAttribute("class", "popup-slide singleSlide slideInLeft")
+    document
+        .getElementById("slide" + currentSlideIndex)
+        .setAttribute("class", "popup-slide singleSlide slideOutRight")
 
-              // Add the appropriate animation class to the slide
-              document.getElementById("slide" + nextSlideIndex).setAttribute("class", "popup-slide singleSlide slideInLeft");
-              document.getElementById("slide" + currentSlideIndex).setAttribute("class", "popup-slide singleSlide slideOutRight");
+    // Set current slide to the new current slide
+    currentSlideIndex = nextSlideIndex
+  }
 
-              // Set current slide to the new current slide
-              currentSlideIndex = nextSlideIndex;
-            };
+  // Navigates to the next slide in the list
+  function nextSlide() {
+    // Figure out what the next slide is
+    var nextSlideIndex
+    // If we are at the last slide the next one is the first (zero based)
+    if (currentSlideIndex === slideArray.length - 1) {
+      nextSlideIndex = 0
+    } else {
+      // Otherwise the next slide is the current one plus 1
+      nextSlideIndex = currentSlideIndex + 1
+    }
 
-            Slide = function Slide(elemID) {
-              this.elem = $(elemID);
-            };
+    // Setup the next slide and current slide for animations
+    document.getElementById("slide" + nextSlideIndex).style.left = "100%"
+    document.getElementById("slide" + currentSlideIndex).style.left = 0
 
-            // Used to add a numeric id on slide creation to let us target the element later
-            slideIndex = 0;
-            // Tells us which slide we are on
+    // Add the appropriate animation class to the slide
+    document
+        .getElementById("slide" + nextSlideIndex)
+        .setAttribute("class", "popup-slide singleSlide slideInRight")
+    document
+        .getElementById("slide" + currentSlideIndex)
+        .setAttribute("class", "popup-slide singleSlide slideOutLeft")
 
-            currentSlideIndex = 0;
-            // An Array to hold all the slides
+    // Set current slide to the new current slide
+    currentSlideIndex = nextSlideIndex
+  }
 
-            slideArray = [];
+  let counter = currentSlideIndex + 1
+  $(".popup-text-controller-counter").html(`${counter}/${slideArray.length}`)
+  var left = $(".popup-text-controller-left")
+  var right = $(".popup-text-controller-right")
 
+  left.click(async function() {
+    prevSlide()
+    let counter = currentSlideIndex + 1
+    $(".popup-text-controller-counter").html(`${counter}/${slideArray.length}`)
+  })
 
-            // Creating our slide objects, you can make as many as you want
-            slideObjs = $(".popup-slide");
+  right.click(async function() {
+    nextSlide()
+    let counter = currentSlideIndex + 1
+    $(".popup-text-controller-counter").html(`${counter}/${slideArray.length}`)
+  })
+}
 
-            slideObjs.each(function () {
-              $(this).attr("id", "slide" + slideIndex);
-              slideIndex++;
-              var slideTemp = new Slide($(this).attr("id"));
-              slideArray.push(slideTemp);
-            });
-
-            document.getElementById("slide" + currentSlideIndex).style.left = 0;
-
-            // Navigates to the previous slide in the list
-
-
-            // Navigates to the next slide in the list
-            counter = currentSlideIndex + 1;
-
-            $(".popup-text-controller-counter").html(counter + "/" + slideArray.length);
-            left = $(".popup-text-controller-left");
-            right = $(".popup-text-controller-right");
-
-
-            left.click(_asyncToGenerator( /*#__PURE__*/_regeneratorRuntime.mark(function _callee() {
-              var counter;
-              return _regeneratorRuntime.wrap(function _callee$(_context) {
-                while (1) {
-                  switch (_context.prev = _context.next) {
-                    case 0:
-                      prevSlide();
-                      counter = currentSlideIndex + 1;
-
-                      $(".popup-text-controller-counter").html(counter + "/" + slideArray.length);
-
-                    case 3:
-                    case "end":
-                      return _context.stop();
-                  }
-                }
-              }, _callee, this);
-            })));
-
-            right.click(_asyncToGenerator( /*#__PURE__*/_regeneratorRuntime.mark(function _callee2() {
-              var counter;
-              return _regeneratorRuntime.wrap(function _callee2$(_context2) {
-                while (1) {
-                  switch (_context2.prev = _context2.next) {
-                    case 0:
-                      nextSlide();
-                      counter = currentSlideIndex + 1;
-
-                      $(".popup-text-controller-counter").html(counter + "/" + slideArray.length);
-
-                    case 3:
-                    case "end":
-                      return _context2.stop();
-                  }
-                }
-              }, _callee2, this);
-            })));
-
-          case 15:
-          case "end":
-            return _context3.stop();
-        }
-      }
-    }, _callee3, this);
-  }));
-
-  return function initSlider() {
-    return _ref.apply(this, arguments);
-  };
-}();
-
-function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
-
-export default initSlider;
+export default initSlider
